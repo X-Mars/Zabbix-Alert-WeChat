387afb8b5277a357095b4ea4770848d38248a16d
火星小刘 <xtlyk@126.com>
2023-09-08 12:40:35 +0800

    Update dingtalk-robot.py

--- dingtalk-robot.py
+++ dingtalk-robot.py
@@ -5,20 +5,21 @@
 
 import sys, requests, json, time, hmac, hashlib, base64, urllib.parse
 
+subject = str(sys.argv[1])
+message = str(sys.argv[2])
+# robot_token 为机器人地址中的xxxxxxxxxxx   https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxx
+robot_token = str(sys.argv[3])
 # 钉钉接口加密鉴权
 # secret 在机器人管理页面设置，勾选加签后，复制秘钥
+secret = str(sys.argv[4])
+
 timestamp = str(round(time.time() * 1000))
-secret = '需要替换这里'
 secret_enc = secret.encode('utf-8')
 string_to_sign = '{}\n{}'.format(timestamp, secret)
 string_to_sign_enc = string_to_sign.encode('utf-8')
 hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
 sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
 
-subject = str(sys.argv[1])
-message = str(sys.argv[2])
-# robot_token 为机器人地址中的xxxxxxxxxxx   https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxx
-robot_token = str(sys.argv[3])
 
 robot = "https://oapi.dingtalk.com/robot/send?access_token=" + robot_token + "&timestamp=" + timestamp + "&sign=" + sign
 
